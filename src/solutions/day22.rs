8e18baee36dd50bdc9bfa7a4d2a58539371ba6f8
Stephen Weinberg <stephenmweinberg@gmail.com>
2022-12-22 20:16:14 -0500

    Add day 22 part 2

--- src/solutions/day22.rs
+++ src/solutions/day22.rs
@@ -27,52 +27,60 @@ pub fn problem1(input: &str) -> Result<String, anyhow::Error> {
         }
     }
 
-    println!("{}", debug_map(&map, &m));
-
     let ans = 1000 * (cur.y + 1) + 4 * (cur.x + 1) + direction_value(dir);
     Ok(ans.to_string())
 }
 
-fn debug_map(map: &Map, points: &HashMap<Point, Direction>) -> String {
-    let mut ret = String::new();
-
-    for (y, row) in map.cells.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            let p = Point::new(x, y);
-            ret.push(match points.get(&p) {
-                Some(Direction::Right) => '>',
-                Some(Direction::Down) => 'v',
-                Some(Direction::Left) => '<',
-                Some(Direction::Up) => '^',
-                None => match cell {
-                    Cell::Empty => ' ',
-                    Cell::Walkable => '.',
-                    Cell::Wall => '#',
-                },
-            });
-        }
-        ret.push('\n');
-    }
-
-    ret
+pub fn problem2(input: &str) -> Result<String, anyhow::Error> {
+    problem2_(input, false)
 }
 
-pub fn problem2(_input: &str) -> Result<String, anyhow::Error> {
-    todo!()
+fn problem2_(input: &str, is_test: bool) -> Result<String, anyhow::Error> {
+    let (map, instructions) = parse!(input);
+
+    let mut walker = CubeWalker {
+        sides: if is_test { SIDES_TEST } else { SIDES },
+        cube_length: if is_test { 4 } else { 50 },
+        direction: Direction::Right,
+        side: 1,
+        point: Point::new(0, 0),
+    };
+
+    let mut m = HashMap::new();
+    m.insert(walker.as_point(), walker.direction);
+
+    for inst in instructions {
+        match inst {
+            Instruction::Turn(t) => walker = walker.turn(t),
+            Instruction::Step(n) => {
+                for _ in 0..n {
+                    let next = walker.step();
+                    if map.get(&next.as_point()).unwrap() == Cell::Wall {
+                        break;
+                    }
+                    walker = next;
+                    m.insert(walker.as_point(), walker.direction);
+                }
+            }
+        }
+    }
+
+    let cur = walker.as_point();
+    let ans = 1000 * (cur.y + 1) + 4 * (cur.x + 1) + direction_value(walker.direction);
+    Ok(ans.to_string())
 }
 
 fn direction_value(d: Direction) -> usize {
     match d {
         Direction::Right => 0,
-        Direction::Down => 1,
+        Direction::Up => 1,
         Direction::Left => 2,
-        Direction::Up => 3,
+        Direction::Down => 3,
     }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
-    Empty,
     Walkable,
     Wall,
 }
@@ -109,16 +117,12 @@ pub enum Instruction {
 }
 
 pub struct Map {
-    cells: Vec<Vec<Cell>>,
+    cells: Vec<Vec<Option<Cell>>>,
 }
 
 impl Map {
     pub fn get(&self, p: &Point) -> Option<Cell> {
-        self.cells.get(p.y)?.get(p.x).copied()
-    }
-
-    pub fn get_mut(&mut self, p: &Point) -> Option<&mut Cell> {
-        self.cells.get_mut(p.y)?.get_mut(p.x)
+        self.cells.get(p.y)?.get(p.x).copied().flatten()
     }
 
     // Behavior is undefined if you step from an invalid starting location
@@ -168,7 +172,7 @@ impl Map {
     }
 
     fn is_valid_location(&self, p: &Point) -> bool {
-        self.get(p).unwrap_or(Cell::Empty) != Cell::Empty
+        self.get(p).is_some()
     }
 
     fn starting_location(&self) -> Point {
@@ -181,15 +185,267 @@ impl Map {
     }
 }
 
+#[derive(Clone, Debug)]
+struct CubeWalker {
+    sides: [Side; 7],
+    cube_length: usize,
+    direction: Direction,
+    side: u8,
+    point: Point,
+}
+
+impl CubeWalker {
+    fn step(&self) -> CubeWalker {
+        let next = self
+            .point
+            .next(self.direction)
+            .filter(|p| p.x < self.cube_length && p.y < self.cube_length);
+
+        if let Some(next) = next {
+            return CubeWalker {
+                sides: self.sides,
+                cube_length: self.cube_length,
+                direction: self.direction,
+                side: self.side,
+                point: next,
+            };
+        }
+
+        let other_coord = match self.direction {
+            Direction::Up | Direction::Down => self.point.x,
+            Direction::Left | Direction::Right => self.point.y,
+        };
+
+        let r = self.sides[self.side as usize].step(self.direction);
+        let other_coord = if r.reverse {
+            self.cube_length - other_coord - 1
+        } else {
+            other_coord
+        };
+
+        self.new_side(r.next_side, r.border, other_coord)
+    }
+
+    fn turn(&self, t: TurnDirection) -> Self {
+        Self {
+            sides: self.sides,
+            cube_length: self.cube_length,
+            direction: t.apply(self.direction),
+            side: self.side,
+            point: self.point,
+        }
+    }
+
+    fn new_side(&self, side: u8, border: SquareBorder, other_coordinate: usize) -> Self {
+        let point = match border {
+            SquareBorder::Top => Point::new(other_coordinate, 0),
+            SquareBorder::Bottom => Point::new(other_coordinate, self.cube_length - 1),
+            SquareBorder::Left => Point::new(0, other_coordinate),
+            SquareBorder::Right => Point::new(self.cube_length - 1, other_coordinate),
+        };
+
+        Self {
+            sides: self.sides,
+            cube_length: self.cube_length,
+            direction: border.inward(),
+            side,
+            point,
+        }
+    }
+
+    fn as_point(&self) -> Point {
+        let index = self.sides[self.side as usize].index;
+        Point {
+            x: index.x * self.cube_length + self.point.x,
+            y: index.y * self.cube_length + self.point.y,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct SideRelation {
+    next_side: u8,
+    border: SquareBorder,
+    reverse: bool,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Side {
+    index: Point,
+
+    left: SideRelation,
+    right: SideRelation,
+    top: SideRelation,
+    bottom: SideRelation,
+}
+
+impl Side {
+    fn step(&self, d: Direction) -> &SideRelation {
+        match d {
+            Direction::Up => &self.bottom,
+            Direction::Down => &self.top,
+            Direction::Left => &self.left,
+            Direction::Right => &self.right,
+        }
+    }
+}
+
+macro_rules! r {
+    ($next:expr, $border:ident, $reverse:expr) => {
+        SideRelation {
+            next_side: $next,
+            border: SquareBorder::$border,
+            reverse: $reverse,
+        }
+    };
+}
+
+const SIDES: [Side; 7] = [
+    // Dummy
+    Side {
+        index: Point { x: 0, y: 0 },
+        left: r!(0, Top, false),
+        top: r!(0, Top, false),
+        right: r!(0, Top, false),
+        bottom: r!(0, Top, false),
+    },
+    // Side 1
+    Side {
+        index: Point { x: 1, y: 0 },
+        left: r!(5, Left, true),
+        top: r!(6, Left, false),
+        right: r!(2, Left, false),
+        bottom: r!(3, Top, false),
+    },
+    // Side 2
+    Side {
+        index: Point { x: 2, y: 0 },
+        left: r!(1, Right, false),
+        top: r!(6, Bottom, false),
+        right: r!(4, Right, true),
+        bottom: r!(3, Right, false),
+    },
+    // Side 3
+    Side {
+        index: Point { x: 1, y: 1 },
+        left: r!(5, Top, false),
+        top: r!(1, Bottom, false),
+        right: r!(2, Bottom, false),
+        bottom: r!(4, Top, false),
+    },
+    // Side 4
+    Side {
+        index: Point { x: 1, y: 2 },
+        left: r!(5, Right, false),
+        top: r!(3, Bottom, false),
+        right: r!(2, Right, true),
+        bottom: r!(6, Right, false),
+    },
+    // Side 5
+    Side {
+        index: Point { x: 0, y: 2 },
+        left: r!(1, Left, true),
+        top: r!(3, Left, false),
+        right: r!(4, Left, false),
+        bottom: r!(6, Top, false),
+    },
+    // Side 6
+    Side {
+        index: Point { x: 0, y: 3 },
+        left: r!(1, Top, false),
+        top: r!(5, Bottom, false),
+        right: r!(4, Bottom, false),
+        bottom: r!(2, Top, false),
+    },
+];
+
+const SIDES_TEST: [Side; 7] = [
+    // Dummy
+    Side {
+        index: Point { x: 0, y: 0 },
+        left: r!(0, Top, false),
+        top: r!(0, Top, false),
+        right: r!(0, Top, false),
+        bottom: r!(0, Top, false),
+    },
+    // Side 1
+    Side {
+        index: Point { x: 2, y: 0 },
+        left: r!(3, Top, false),
+        top: r!(2, Top, true),
+        right: r!(6, Right, true),
+        bottom: r!(4, Top, false),
+    },
+    // Side 2
+    Side {
+        index: Point { x: 0, y: 1 },
+        left: r!(6, Bottom, true),
+        top: r!(1, Top, true),
+        right: r!(3, Left, false),
+        bottom: r!(5, Bottom, true),
+    },
+    // Side 3
+    Side {
+        index: Point { x: 1, y: 1 },
+        left: r!(2, Right, false),
+        top: r!(1, Left, false),
+        right: r!(4, Left, false),
+        bottom: r!(5, Left, true),
+    },
+    // Side 4
+    Side {
+        index: Point { x: 2, y: 1 },
+        left: r!(3, Right, false),
+        top: r!(1, Bottom, false),
+        right: r!(6, Top, true),
+        bottom: r!(5, Top, false),
+    },
+    // Side 5
+    Side {
+        index: Point { x: 2, y: 2 },
+        left: r!(3, Bottom, true),
+        top: r!(4, Bottom, false),
+        right: r!(6, Left, false),
+        bottom: r!(2, Bottom, true),
+    },
+    // Side 6
+    Side {
+        index: Point { x: 3, y: 2 },
+        left: r!(5, Right, false),
+        top: r!(4, Right, true),
+        right: r!(1, Right, true),
+        bottom: r!(2, Left, true),
+    },
+];
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum SquareBorder {
+    Top,
+    Bottom,
+    Left,
+    Right,
+}
+
+impl SquareBorder {
+    fn inward(&self) -> Direction {
+        match self {
+            Self::Top => Direction::Up,
+            Self::Bottom => Direction::Down,
+            Self::Left => Direction::Right,
+            Self::Right => Direction::Left,
+        }
+    }
+}
+
 mod parser {
     use super::*;
     use crate::parser::prelude::*;
 
     pub fn parse(input: &str) -> IResult<&str, (Map, Vec<Instruction>)> {
         let cell = alt((
-            value(Cell::Empty, char(' ')),
-            value(Cell::Walkable, char('.')),
-            value(Cell::Wall, char('#')),
+            value(None, char(' ')),
+            value(Some(Cell::Walkable), char('.')),
+            value(Some(Cell::Wall), char('#')),
         ));
         let map_line = terminated(many1(cell), line_ending);
         let map = many1(map_line).map(|cells| Map { cells });
@@ -236,6 +492,6 @@ mod tests {
 
     #[test]
     fn problem2_test() {
-        //assert_eq!(problem2(EXAMPLE_INPUT).unwrap(), "5031")
+        assert_eq!(problem2_(EXAMPLE_INPUT, true).unwrap(), "5031")
     }
 }
